40e09ac92411bcfbe0b3a3c8a8e54569c55ff4c1
WambuaJoe <kaytujohn254@gmail.com>
2023-10-09 04:23:13 +0300

    update directory

--- scripts/example1.py
+++
@@ -1,4 +0,0 @@
-MY_EX1_STRING = 'Welcome to Example1 module'
-
-def yolo(x: int):
-    print("You only LIve {} times".format(x))
--- scripts/example2.py
+++
@@ -1,5 +0,0 @@
-import example1
-
-print("The imported string is:\n{}".format(example1.MY_EX1_STRING))
-
-example1.yolo(10)
