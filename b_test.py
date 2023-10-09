b54979fbefc974efd33d689a7b3b1a918126028b
WambuaJoe <kaytujohn254@gmail.com>
2023-10-09 05:10:27 +0300

    create doctest

--- b_test.py
+++ b_test.py
@@ -1,27 +1,5 @@
-def count_vowels(word):
+def raiseError():
     """
-    Given a single word, return the total number of vowels in that single word.
-
-    :param word: str
-    :return: int
-
-    >>> count_vowels('Cusco')
-    2
-
-    >>> count_vowels('Manila')
-    3
-    >>> count_vowels('Islamabad')
-    4
-    >>> count_vowels('Chernobyl')
-    3
+        this function always raises an Exception
     """
-    total_vowels = 0
-    for letter in word.lower():
-        if letter in 'aeiou':
-            total_vowels += 1
-    return total_vowels
-
-
-if __name__ == "__main__":
-    import doctest
-    doctest.testmod(name='count_vowels', verbose=True)
+    raise RuntimeError("Here is the error")
