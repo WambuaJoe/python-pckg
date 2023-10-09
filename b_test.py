f79601f1840b6a3ec9c893dbbbe662aa4d046796
WambuaJoe <kaytujohn254@gmail.com>
2023-10-09 04:16:57 +0300

    implement doctests

--- b_test.py
+++ b_test.py
@@ -1,93 +1,26 @@
-# class Employee:
-#
-#     raise_amt = 1.04
-#
-#     def __init__(self, first, last, pay):
-#         self.first = first
-#         self.last = last
-#         self.pay = pay
-#         self.email = "{}.{}@email.com".format(self.first, self.last)
-#
-#     def fullname(self):
-#         return "{} {}".format(self.first, self.last)
-#
-#     def apply_raise(self):
-#         self.pay = int(self.pay * self.raise_amt)
-#
-#     def __repr__(self):
-#         return "Employee('{}', '{}', '{}')".format(self.first,
-#                                                    self.last, self.pay)
-#
-#     def __str__(self):
-#         return "{} - {}".format(self.fullname(), self.email)
-#
-#     def __add__(self, other):
-#         return "{}".format(format((self.pay + other.pay), ','))
-#
-#     def __len__(self):
-#         return len(self.fullname())
-#
-#
-# def main():
-#
-#     emp_1 = Employee('Test', 'User', 55000)
-#     emp_2 = Employee('Joe', 'Wambua', 45000)
-#
-#     # print(emp_1)
-#     print(emp_1 + emp_2)
-#
-#
-# main()
+def count_vowels(word):
+    """
+    Given a single word, return the total number of vowels in that single word.
 
+    :param word: str
+    :return: int
 
-# def dec_funct(origin_funct):
-#     def wrap_funct(*args, **kwargs):
-#         print("Wrapper exec'd this before {}()".format(origin_funct.__name__))
-#         return origin_funct(*args, **kwargs)
-#     return wrap_funct
+    >>> count_vowels('Cusco')
+    2
 
+    >>> count_vowels('Manila')
+    3
+    >>> count_vowels('Islamabad')
+    4
+    >>> count_vowels('Chernobyl')
+    3
+    """
+    total_vowels = 0
+    for letter in word.lower():
+        if letter in 'aeiou':
+            total_vowels += 1
+    return total_vowels
 
-# @dec_funct
-# def display():
-#     print("Display function OK")
-from functools import wraps
-
-
-def my_log(orig_funct):
-    import logging
-    logging.basicConfig(filename="{}.log"
-                        .format(orig_funct.__name__), level=logging.INFO)
-
-    @wraps(orig_funct)
-    def wrapper(*args, **kwargs):
-        logging.info(
-            "Ran with args: {}, & kwargs: {}".format(args, kwargs))
-        return orig_funct(*args, **kwargs)
-
-    return wrapper
-
-
-def my_timer(orig_funct):
-    import time
-
-    @wraps(orig_funct)
-    def wrapper(*args, **kwargs):
-        t1 = time.time()
-        result = orig_funct(*args, **kwargs)
-        t2 = time.time() - t1
-        print("{} ran in {:.8f} sec".format(orig_funct.__name__, t2))
-        return result
-
-    return wrapper()
-
-
-@my_log
-# @my_timer
-def info_displ(name):
-    import time
-
-    time.sleep(1)
-    print("info_displ ran with args ({})".format(name))
-
-
-info_displ('Milan')
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod(name='count_vowels', verbose=True)
--- integer.py
+++ integer.py
@@ -1,25 +1,9 @@
-#!/usr/bin/python3
-"""
-Addd integer module
-contains:
-add_int function
-"""
-from b_test import my_log
+import datetime
 
+mydate = datetime.datetime.now()
 
-@my_log
-def add_int(a, b=5):
-    """
-    computes and returns the addition of 2 integers
-    """
-    if type(a) is float:
-        a = int(a)
-    if type(b) is float:
-        b = int(b)
+print("__str__(): ", mydate.__str__)
+print("str(): ", str(mydate))
 
-    if not isinstance(a, int):
-        raise TypeError("a must be an integer")
-    if not isinstance(b, int):
-        raise TypeError("b must be an integer")
-
-    return a + b
+print("__repr__(): ", mydate.__repr__)
+print("repr(): ", repr(mydate))
