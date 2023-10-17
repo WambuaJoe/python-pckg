fdecc353954523147c45f7d3cc48815cc17e7d7d
WambuaJoe <kaytujohn254@gmail.com>
2023-10-17 18:52:36 +0300

    code update

--- b_test.py
+++ b_test.py
@@ -1,41 +1,16 @@
-class Polygon:
-    # Initializing the number of sides
-    def __init__(self, no_of_sides):
-        self.n = no_of_sides
-        self.sides = [0 for i in range(no_of_sides)]
-
-    def inputSides(self):
-        self.sides = [float(input("Enter side "+str(i+1)+" : ")) for i in range(self.n)]
-
-    # method to display the length of each side of the polygon
-    def dispSides(self):
-        for i in range(self.n):
-            print("Side",i+1,"is",self.sides[i])
-
-class Triangle(Polygon):
-    # Initializing the number of sides of the triangle to 3 by 
-    # calling the __init__ method of the Polygon class
+class base():
     def __init__(self):
-        Polygon.__init__(self,3)
+        print("In the base class")
+        self.s1 = "base class vara"
 
-    def findArea(self):
-        a, b, c = self.sides
+class derived(base):
+    def __init__(self):
+        super().__init__()
+        print("In the derived class")
+        self.s2= "derived class var"
+        # super().__init__()
 
-        # calculate the semi-perimeter
-        s = (a + b + c) / 2
 
-        # Using Heron's formula to calculate the area of the triangle
-        area = (s*(s-a)*(s-b)*(s-c)) ** 0.5
-        print('The area of the triangle is %0.2f' %area)
-
-# Creating an instance of the Triangle class
-t = Triangle()
-
-# Prompting the user to enter the sides of the triangle
-t.inputSides()
-print()
-# Displaying the sides of the triangle
-t.dispSides()
-print()
-# Calculating and printing the area of the triangle
-t.findArea()
+a=derived()
+print(a.s1)
+print(a.s2)
