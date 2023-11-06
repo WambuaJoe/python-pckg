6531e62b348a0b7483ba967f50d5c7db346b08ec
Unpopular Bonobo <58861284+WambuaJoe@users.noreply.github.com>
2023-11-06 08:34:24 +0000

    code update

--- cmd_line.py
+++ cmd_line.py
@@ -4,24 +4,24 @@ import cmd
 
 class HelloWorld(cmd.Cmd):
     """ simple command processor """
-    # MABONZO = ['sean', 'kelly', 'mutua', 'mwangi', 'ian', 'nate']
+    MABONZO = ['sean', 'kelly', 'mutua', 'mwangi', 'ian', 'nate']
 
-    # def do_greet(self, person):
-    #     """ greet [person] """
-    #     if person.lower and person.lower in self.MABONZO:
-    #         greeting = f'{person.title()}, niaje we matako'
-    #     elif person:
-    #         greeting = f'{person.title()}, niaje'
-    #     else:
-    #         greeting = 'niaje'
-    #     print(greeting)
+    def do_greet(self, person):
+        """ greet [person] """
+        if person.lower and person.lower in self.MABONZO:
+            greeting = f'{person.title()}, niaje we matako'
+        elif person:
+            greeting = f'{person.title()}, niaje'
+        else:
+            greeting = 'niaje'
+        print(greeting)
 
-    # def complete_greet(self, text, line, begidx, endidx):
-    #     if not text:
-    #         completions = self.MABONZO[:]
-    #     else:
-    #         completions = [ f for f in self.MABONZO if f.startswith(text)]
-    #     return completions
+    def complete_greet(self, text, line, begidx, endidx):
+        if not text:
+            completions = self.MABONZO[:]
+        else:
+            completions = [ f for f in self.MABONZO if f.startswith(text)]
+        return completions
 
     # def help_greet(self):
     #     print('\n'.join([
@@ -50,4 +50,3 @@ class HelloWorld(cmd.Cmd):
 
 if __name__ == "__main__":
     HelloWorld().cmdloop()
-
