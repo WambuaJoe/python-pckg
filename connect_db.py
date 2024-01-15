8e762324b7ab14750cb8d2faeeec716a5b88b7ac
WambuaJoe <kaytujohn254@gmail.com>
2024-01-15 20:40:28 +0300

    execute multiple substitution queries

--- connect_db.py
+++ connect_db.py
@@ -6,32 +6,44 @@ import MySQLdb
 from sys import argv
 
 if __name__ == "__main__":
-    # connect to database
-    try:
-        db = MySQLdb.connect(
+    # connect to dB
+    db = MySQLdb.connect(
             host="localhost",
             port=3306, user="root",
             passwd="chr0n!ken",
-            db="bank_schema")
+            db="test_db")
 
-        _cursor = db.cursor()
-        
-        # run query that might raise an error
-        _cursor.execute("SELECT * FROM customer")
-        
-        db.commit()
-    # 
-    except MySQLdb.Error as error:
-        error_code = error.args[0]
+    _cursor = db.cursor()
+    # connect to database
+    # try:
+    #     db = MySQLdb.connect(
+    #         host="localhost",
+    #         port=3306, user="root",
+    #         passwd="chr0n!ken",
+    #         db="bank_schema")
 
-        if error_code == 1146:
-            print("table doesn't exist")
-        elif error_code == 1045:
-            print("Access denied: invalid user authentication")
-        else:
-            print(f"Error {error_code}: {error}")
+    #     _cursor = db.cursor()
+        
+    #     # run query that might raise an error
+    #     _cursor.execute("SELECT * FROM customer")
+        
+    #     db.commit()
+    # # 
+    # except MySQLdb.Error as error:
+    #     error_code = error.args[0]
+
+    #     if error_code == 1146:
+    #         print("table doesn't exist")
+    #     elif error_code == 1045:
+    #         print("Access denied: invalid user authentication")
+    #     else:
+    #         print(f"Error {error_code}: {error}")
     
-    finally:
-        _cursor.close()
-        db.close()
+    # finally:
+    #     _cursor.close()
+    #     db.close()
 
+    # create table
+    # _cursor.execute("CREATE TABLE song (id INT UNSIGNED PRIMARY KEY AUTO_INCREMENT, title TEXT NOT NULL)")
+    
+    
